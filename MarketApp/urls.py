404df73d7a485cf03d5949190fef130b71fb19fc
Adel5002 <aromat29.11@gmail.com>
2023-12-20 20:04:26 +0300

    Header, ListView, DetailView and stylings was added

--- MarketApp/urls.py
+++ MarketApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ItemListView, ItemDetailView
 
 urlpatterns = [
-
+    path('', ItemListView.as_view(), name='items_list'),
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='item_details'),
 ]
--- MarketApp/views.py
+++ MarketApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Item
+
+
+class ItemListView(ListView):
+    model = Item
+    context_object_name = 'items'
+    template_name = 'MarketApp/items.html'
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    context_object_name = 'item'
+    template_name = 'MarketApp/item_details.html'
+
+
+
+
+
